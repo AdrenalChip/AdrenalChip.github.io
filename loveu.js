4454e876f59b284a844bf632827d9412712035e8
Jorge Guerrero Díaz <jorgeguerrerodiaz909@gmail.com>
2022-02-17 23:09:30 -0600

    Add files via upload

---
+++ loveu.js
@@ -0,0 +1,6 @@
+alert("\tBienvenida\n")
+alert("Tu eres el amor de mi vida <3")
+const respuesta=confirm("Si lo sabes, verdad?");
+alert("\tEres lo que más amo en el mundo \n")
+const respuesta2=confirm("Sabes que me voy a casar contigo?");
+alert("\t Esta es la primera version de muchas\n");
